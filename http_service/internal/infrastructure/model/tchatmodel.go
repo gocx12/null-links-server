93c32679d3b45410cfac749fd9efdca10068c3b5
gaochengxi <gaochengxi12@qq.com>
2024-11-03 12:38:03 +0800

    fix

--- http_service/internal/infrastructure/model/tchatmodel.go
+++ http_service/internal/infrastructure/model/tchatmodel.go
@@ -16,6 +16,7 @@ type (
 		withSession(session sqlx.Session) TChatModel
 		FindChatList(ctx context.Context, websetId int64, page, pageSize int32) ([]*TChat, error)
 		FindChatListChatId(ctx context.Context, websetId, lastChatId int64, page, pageSize int32) ([]*TChat, error)
+		FindChatListByTopicId(ctx context.Context, TopicId int64, page, pageSize int32) ([]*TChat, error)
 	}
 
 	customTChatModel struct {
@@ -50,6 +51,18 @@ func (c *customTChatModel) FindChatListChatId(ctx context.Context, websetId, las
 	query := "select * from t_chat where webset_id = ? and chat_id < ? order by created_at desc limit ?, ?"
 	var resp []*TChat
 	err := c.conn.QueryRowsCtx(ctx, &resp, query, websetId, lastChatId, (page-1)*pageSize, pageSize)
+	switch err {
+	case nil:
+		return resp, nil
+	default:
+		return nil, err
+	}
+}
+
+func (c *customTChatModel) FindChatListByTopicId(ctx context.Context, TopicId int64, page, pageSize int32) ([]*TChat, error) {
+	query := "select * from t_chat where topic_id = ? order by created_at desc limit ?, ?"
+	var resp []*TChat
+	err := c.conn.QueryRowsCtx(ctx, &resp, query, TopicId, (page-1)*pageSize, pageSize)
 	switch err {
 	case nil:
 		return resp, nil
--- http_service/internal/logic/chat/gettopiclogic.go
+++ http_service/internal/logic/chat/gettopiclogic.go
@@ -31,12 +31,46 @@ func (l *GetTopicLogic) GetTopic(req *types.ChatGetTopicReq) (resp *types.ChatGe
 		logx.Error("getTopic error | err=", err)
 		resp.StatusCode = internal.StatusErr
 		resp.StatusMsg = "get topic error"
+		return
 	}
 
 	resp.ChatList = make([]types.Chat, 0)
-	chatListDb, err := l.svcCtx.ChatModel.FindByTopicId(l.ctx, req.TopicId)
+	chatListDb, err := l.svcCtx.ChatModel.FindChatListByTopicId(l.ctx, req.TopicId, 0, 10)
+	if err != nil {
+		logx.Error("getChatListByTopicId error | err=", err)
+		resp.StatusCode = internal.StatusErr
+		resp.StatusMsg = "get topic error"
+		return
+	}
+
+	// user去重
+	userIdSet := make(map[int64]bool)
+	for _, chat := range chatListDb {
+		userIdSet[chat.UserId] = true
+	}
+	userIdList := make([]int64, 0)
+	for userId := range userIdSet {
+		userIdList = append(userIdList, userId)
+	}
+
+	userInfoDb, err := l.svcCtx.UserModel.FindMulti(l.ctx, userIdList)
+	if err != nil {
+		logx.Error("findMulti error | err=", err)
+		resp.StatusCode = internal.StatusErr
+		resp.StatusMsg = "get topic error"
+		return
+	}
 
 	resp.TopicTitle = topicDb.Title
+	resp.ChatList = make([]types.Chat, 0)
+	for _, chat := range chatListDb {
+		resp.ChatList = append(resp.ChatList, types.Chat{
+			ChatID:   chat.Id,
+			Content:  chat.Content,
+			UserID:   chat.UserId,
+			UserName: userInfoDb[chat.UserId].Username,
+		})
+	}
 	resp.StatusCode = internal.StatusSuccess
 	resp.StatusMsg = "success"
 	return
